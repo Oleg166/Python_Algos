6b6d1dc0a1e5206d375b76b6b45972136acc0589
Dementev Oleg <demeoleg@mail.ru>
2020-05-05 15:16:07 +0300

    Частично выполнил задания

--- Урок 5. Практическое задание/task_1.py
+++ Урок 5. Практическое задание/task_1.py
@@ -25,3 +25,76 @@
 
 Предприятия, с прибылью ниже среднего значения: Копыта
 """
+import collections
+"""
+COUNT = int(input("Введите количество предприятий для расчета прибыли: "))
+FIRMA = collections.namedtuple('FIRMA', ['name', 'q1', 'q2', 'q3', 'q4'])
+FIRMA_1 = FIRMA(name=input("Введите название предприятия: "), q1=0, q2=0, q3=0, q4=0)
+FIRMA_1.name = input("Введите название предприятия: ")
+
+FIRMA_2 = FIRMA(name='Копыта', q1=345, q2=34, q3=543, q4=34)
+print(FIRMA_1)
+print(FIRMA_2)
+PROFIT_FIRMA_1 = FIRMA_1.q1 + FIRMA_1.q2 + FIRMA_1.q3 + FIRMA_1.q4
+PROFIT_FIRMA_2 = FIRMA_2.q1 + FIRMA_2.q2 + FIRMA_2.q3 + FIRMA_2.q4
+PROFIT = (PROFIT_FIRMA_1 + PROFIT_FIRMA_2)/2
+print(PROFIT_FIRMA_1)
+print(PROFIT_FIRMA_2)
+print(f"Средняя годовая прибыль всех предприятий: {PROFIT}")
+# A = input("через пробел введите прибыль данного предприятия за каждый квартал(Всего 4 квартала): ")
+# print(input("через пробел введите прибыль данного предприятия за каждый квартал(Всего 4 квартала): ").split(" "))
+FIRMA = collections.namedtuple('FIRMA', ['name', 'q1', 'q2', 'q3', 'q4'])
+NAME_1 = input("Введите название предприятия: ")
+PROFIT = input("через пробел введите прибыль данного предприятия за каждый квартал(Всего 4 квартала): ").split(" ")
+FIRMA_1 = FIRMA(name=NAME_1, q1=PROFIT[0], q2=PROFIT[1], q3=PROFIT[2], q4=PROFIT[3])
+print(FIRMA_1)
+
+FIRMA = collections.namedtuple('FIRMA', ['name', 'profit'])
+#NAME_I = input("Введите название предприятия: ")
+PROFIT_I = input("через пробел введите прибыль данного предприятия за каждый квартал(Всего 4 квартала): ").split(" ")
+_sum = 0
+for i in PROFIT_I:
+    _sum = _sum + int(i)
+print(_sum)
+FIRMA_1 = FIRMA(name=NAME_I, profit=PROFIT_I)
+print(FIRMA_1)
+print(sum(PROFIT_I))
+print(f"Название фирмы: {FIRMA_1.name}")
+print(f"Прибыль фирмы за год: {sum(FIRMA_1.profit)}")"""
+
+FIRMA = collections.namedtuple('FIRMA', ['name', 'profit'])
+COUNT = int(input("Введите количество предприятий для расчета прибыли: "))
+PROFIT_MEAN = 0
+LIST_1 = []
+for i in range(1, COUNT+1):
+    NAME_F = input("Введите название предприятия: ")
+    PROFIT_I = input("через пробел введите прибыль данного предприятия за каждый квартал(Всего 4 квартала): ").split(
+        " ")
+    PROFIT_Y = 0
+    for j in PROFIT_I:
+        PROFIT_Y = PROFIT_Y + int(j)
+    LIST_1.append(FIRMA(name=NAME_F, profit=PROFIT_Y))
+
+# print(LIST_1)
+
+_sum = 0
+for i in range(0, len(LIST_1)):
+    _sum = _sum + int(LIST_1[i][1])
+
+_sum_2 = _sum / len(LIST_1)
+print(f"Средняя годовая прибыль всех предприятий: {_sum_2}")
+LOW = []
+HIGH = []
+MEAN = []
+for j in range(0, len(LIST_1)):
+    if int(LIST_1[j][1]) > _sum_2:
+        HIGH.append(LIST_1[j][0])
+    elif int(LIST_1[j][1]) < _sum_2:
+        LOW.append(LIST_1[j][0])
+    else:
+        MEAN.append(LIST_1[j][0])
+
+print(f"Предприятия, с прибылью выше среднего значения: {' '.join(HIGH)}")
+print(f"Предприятия, с прибылью ниже среднего значения: {' '.join(LOW)}")
+
+print(" ".join(MEAN))
--- Урок 5. Практическое задание/task_2.py
+++ Урок 5. Практическое задание/task_2.py
@@ -11,3 +11,29 @@
 Также попробуйте решить задачу вообще без collections и применить только ваши знания по ООП
 (в частности по перегрузке методов)
 """
+import collections
+
+FIRMA = collections.namedtuple('FIRMA', ['name', 'profit'])
+LIST_1 = [FIRMA(name='A', profit=4), FIRMA(name='B', profit=8), FIRMA(name='C', profit=14)]
+
+
+_sum = 0
+for i in range(0, len(LIST_1)):
+    _sum = _sum + int(LIST_1[i][1])
+
+_sum_2 = _sum / len(LIST_1)
+print(_sum_2)
+LOW = []
+HIGH = []
+MEAN = []
+for j in range(0, len(LIST_1)):
+    if int(LIST_1[j][1]) > _sum_2:
+        LOW.append(LIST_1[j][0])
+    elif int(LIST_1[j][1]) < _sum_2:
+        HIGH.append(LIST_1[j][0])
+    else:
+        MEAN.append(LIST_1[j][0])
+
+print(" ".join(LOW))
+print(" ".join(HIGH))
+print(" ".join(MEAN))
